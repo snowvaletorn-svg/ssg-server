7cf5ffee6806fddec0aad61c9fb7599077f3fdc3
Snowvale <snowvaletorn@gmail.com>
2026-04-01 21:28:10 -0400

    Fixes Port

--- server.js
+++ server.js
@@ -217,7 +217,7 @@ app.use(session({
 passport.use(new DiscordStrategy({
   clientID: process.env.DISCORD_CLIENT_ID,
   clientSecret: process.env.DISCORD_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/auth/discord/callback',
+  callbackURL: process.env.DISCORD_CALLBACK_URL || 'http://localhost:3000/auth/discord/callback',
   scope: ['identify', 'email', 'guilds', 'guilds.members.read']
 },
   async (accessToken, refreshToken, profile, done) => {
