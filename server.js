f892872ae3789b66c92c39304eec845a1a254529
Snowvale <snowvaletorn@gmail.com>
2026-05-07 19:04:16 -0400

    fixed application emails

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const mongoose = require('mongoose');
 const rateLimit = require('express-rate-limit');
 const compression = require('compression');
 const nodemailer = require('nodemailer');
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
 const factionData = require('./data/factions');
 const User = require('./models/User');
 const FactionConfig = require('./models/FactionConfig');
