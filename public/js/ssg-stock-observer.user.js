79914e9c72818c66dac7908be92d8b54285d3eaf
Snowvale <snowvaletorn@gmail.com>
2026-05-22 14:14:05 -0400

    Testing flight observer code.

--- public/js/ssg-stock-observer.user.js
+++ public/js/ssg-stock-observer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SSG Stock Observer
 // @namespace    https://ssg-server.onrender.com
-// @version      1.2.1
+// @version      1.3.0
 // @description  Monitors and submits foreign stock data dynamically using MutationObservers on page changes. PC + Torn PDA friendly.
 // @author       SSG
 // @match        *://*.torn.com/travel.php*
